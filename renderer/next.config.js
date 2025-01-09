fc4795a95537b5019097a26bf2a024a7bfef686d
mumeinosato <mumeinosato@gmail.com>
2025-01-09 18:07:35 +0900

    webpack設定を更新し、rc-utilおよびantdモジュールの完全指定を無効にするルールを追加

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -7,6 +7,20 @@ module.exports = {
     unoptimized: true,
   },
   webpack: (config) => {
+    config.module.rules.push({
+      test: /node_modules\/rc-util\/es/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
+    config.module.rules.push({
+      test: /node_modules\/antd\/es/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
     return config
   },
 }
